ad73b8254d9ff41593b1fb6f5f300355afbfb6d4
Sourabh Shegane <sgsourabh30@gmail.com>
2020-10-17 17:16:43 +0530

    Initial commit

---
+++ app/src/main/java/com/android/learningmvvm/db/Subscriber.kt
@@ -0,0 +1,18 @@
+package com.android.learningmvvm.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "subscriber_data")
+data class Subscriber(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "subscriber_id")
+    val id: Int,
+
+    @ColumnInfo(name = "subscriber_name")
+    val name: String,
+
+    @ColumnInfo(name = "subscriber_email")
+    val email: String){
+}
